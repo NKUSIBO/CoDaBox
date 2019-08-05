70dcd48ffd8dc968007daf3a1b2dd7b72ae41536
Bilal El Khattouti <belkhattouti@inocrea.be>
2019-08-05 12:36:58 +0200

    Changement d'addresse

--- Inocrea.CodaBox.ApiServer/Controllers/TransactionsController.cs
+++ Inocrea.CodaBox.ApiServer/Controllers/TransactionsController.cs
@@ -42,75 +42,13 @@ namespace Inocrea.CodaBox.ApiServer.Controllers
 
             return transactions;
         }
+
         [HttpGet("{statementId}")]
         public async Task<List<Transactions>> GetTransactionsByStatement(int statementId)
         {
             var transactions = await _context.Transactions.Where(i=>i.StatementId==statementId).ToListAsync();
-
-            
-
             return transactions;
         }
 
-        // PUT: api/Transactions/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTransactions(int id, Transactions transactions)
-        {
-            if (id != transactions.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(transactions).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TransactionsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Transactions
-        [HttpPost]
-        public async Task<ActionResult<Transactions>> PostTransactions(Transactions transactions)
-        {
-            _context.Transactions.Add(transactions);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTransactions", new { id = transactions.Id }, transactions);
-        }
-
-        // DELETE: api/Transactions/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Transactions>> DeleteTransactions(int id)
-        {
-            var transactions = await _context.Transactions.FindAsync(id);
-            if (transactions == null)
-            {
-                return NotFound();
-            }
-
-            _context.Transactions.Remove(transactions);
-            await _context.SaveChangesAsync();
-
-            return transactions;
-        }
-
-        private bool TransactionsExists(int id)
-        {
-            return _context.Transactions.Any(e => e.Id == id);
-        }
     }
 }
