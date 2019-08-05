5d16e25d8aace086bf3238d35721f9e92efc6c52
Bilal El Khattouti <belkhattouti@inocrea.be>
2019-08-05 16:41:17 +0200

    Controller balance Deux Get

--- Inocrea.CodaBox.ApiServer/Controllers/CompteBancairesController.cs
+++ Inocrea.CodaBox.ApiServer/Controllers/CompteBancairesController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Inocrea.CodaBox.ApiServer.Entities;
-using Inocrea.CodaBox.ApiServer.ViewModels;
 
 namespace Inocrea.CodaBox.ApiServer.Controllers
 {
@@ -36,35 +32,6 @@ namespace Inocrea.CodaBox.ApiServer.Controllers
             if (compteBancaire == null) return NotFound();
 
             return compteBancaire;
-        }
-
-        // GET: api/CompteBancaires/5/Balance
-        [HttpGet("{id}/Balance")]
-        public async Task<ActionResult<BalanceViewModel>> GetCompteBalannce(int id)
-        {
-            try
-            {
-                var compteBancaire = await _context.CompteBancaire.FindAsync(id);
-                if (compteBancaire == null) { return NotFound(); }
-                var statementID = _context.Statements.Where(s => s.CompteBancaireId == compteBancaire.Id).Max(s => s.StatementId);
-                var statement = _context.Statements.Find(statementID);
-
-                var cb = new BalanceViewModel
-                {
-                    CompteBancaire = compteBancaire,
-                    NewBalance = statement.NewBalance,
-                    InitialBalance = statement.InitialBalance,
-                    Date = statement.Date.ToString("dd/MM/yyyy")
-                };
-
-                cb.Difference = cb.NewBalance - cb.InitialBalance;
-
-                return cb;
-            }
-            catch (Exception e)
-            {
-                return NotFound();
-            }
         }
 
     }
